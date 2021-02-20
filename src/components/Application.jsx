9ddb90b75545f31721244943b3654a33744ce1aa
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-02-20 13:05:48 +0100

    remove dummy posts data

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,52 +2,19 @@ import React, { Component } from 'react';
 
 import Posts from './Posts';
 import { firestore } from '../firebase';
+import { collectIdsAndDocs } from '../utilities';
 
 class Application extends Component {
   state = {
-    posts: [
-      {
-        id: '1',
-        title: 'A Very Hot Take',
-        content:
-          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-        user: {
-          uid: '123',
-          displayName: 'Bill Murray',
-          email: 'billmurray@mailinator.com',
-          photoURL: 'https://www.fillmurray.com/300/300',
-        },
-        stars: 1,
-        comments: 47,
-      },
-      {
-        id: '2',
-        title: 'The Sauciest of Opinions',
-        content:
-          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-        user: {
-          uid: '456',
-          displayName: 'Mill Burray',
-          email: 'notbillmurray@mailinator.com',
-          photoURL: 'https://www.fillmurray.com/400/400',
-        },
-        stars: 3,
-        comments: 0,
-      },
-    ],
+    posts: [],
   };
 
   async componentDidMount() {
     const snapShot = await firestore.collection('posts').get();
 
-    snapShot.forEach(doc => {
-      const id = doc.id;
-      const data = doc.data();
+    const posts = snapShot.docs.map(collectIdsAndDocs);
 
-      console.log(id, data);
-    })
-
-    console.log({ snapShot });
+    this.setState({ posts });
   }
 
   handleCreate = post => {
