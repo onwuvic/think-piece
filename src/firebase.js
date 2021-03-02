d885480940395b26a99f3798a73668fdf5af8536
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-03-02 14:14:29 +0100

    add auth with google, email & password

--- src/firebase.js
+++ src/firebase.js
@@ -19,5 +19,48 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export const firestore = firebase.firestore();
+export const auth = firebase.auth(); 
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signOut = () => auth.signOut();
+export const createUserProfileDocument = async (user, additionalData) => {
+    if (!user) return;
+
+    const userRef = firestore.doc(`users/${user.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email, photoURL } = user;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                photoURL,
+                createdAt,
+                ...additionalData
+            })
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    return getUserDocument(user.uid);
+}
+
+export const getUserDocument = async (uid) => {
+    if(!uid) return null;
+
+    try {
+        const userDocument = await firestore.collection('users').doc(uid).get();
+
+        return { uid, ...userDocument.data() }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
 export default firebase;
