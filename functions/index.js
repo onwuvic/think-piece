854e3c0f11d74f4a141fd2ae2493048cbb4f0f14
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-03-12 12:57:05 +0100

    add cloud functions

---
+++ functions/index.js
@@ -0,0 +1,49 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+exports.getAllPosts = functions.https.onRequest( async (request, reponse) => {
+    const snapShot = await firestore.collection('posts').get();
+    const posts = snapShot.docs.map(doc => ({id: doc.id, ...doc.data()}))
+
+    reponse.json({ posts });
+});
+
+exports.sanitizeContent = functions.firestore
+    .document('posts/${postId}')
+    .onWrite(async (change) => {
+        if (!change.after.exists) return;
+        const { content, sanitized } = change.after.data();
+
+        if (content && !sanitized) {
+            return change.after.ref.update({
+                content: content.replace(/CoffeeScript/g, '*******'),
+                sanitized: true,
+            });
+        }
+
+        return null;
+    });
+
+
+exports.incrementCommentCount = functions.firestore
+    .document('posts/${postId}/comments/{commentId}')
+    .onCreate(async (snapShot, context) => {
+        const { postId } = context.params;
+        const postRef = firestore.doc(`posts/${postId}`);
+
+        const snap = await postRef.get('comments');
+        const comments = snap.get('comments');
+        return postRef.update({ comments: comments + 1 });
+    });
